99e4e4afd755b386cc71e81fcd7775c4c0c18e87
Isyan <isyan@mail.ru>
2022-04-24 19:41:18 +0300

    add modal & animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,40 @@
 // menu script
 const gamburger = document.querySelector('.hanburger'),
 	menu = document.querySelector('.menu'),
-	menuClose = document.querySelector('.menu__close');
+	menuClose = document.querySelector('.menu__close'),
+	menuOverlay = document.querySelector('.menu__overlay');
+
+
+const closeMenu = (modalSelector) => {
+	modalSelector.classList.remove('active')
+}
+const openMenu = (modalSelector) => {
+	modalSelector.classList.add('active')
+}
 
 gamburger.addEventListener('click', () => {
-	menu.classList.add('active');
+	openMenu(menu);
+	// menu.classList.add('active');
+	setTimeout(() => {
+		openMenu(menuClose)
+		// menuClose.classList.add('active');
+	}, 1000)
 })
 menuClose.addEventListener('click', () => {
-	menu.classList.remove('active')
+	closeMenu(menu)
+	closeMenu(menuClose)
 })
+menuOverlay.addEventListener('click', () => {
+	closeMenu(menu)
+	closeMenu(menuClose)
+})
+
+document.addEventListener('keydown', (e) => {
+	if (e.code === "Escape") {
+		closeMenu(menu);
+		closeMenu(menuClose)
+	}
+});
 
 
 
@@ -18,4 +44,49 @@ const progressCounters = document.querySelectorAll('.progress__counter'),
 
 progressCounters.forEach((item, index) => {
 	progressLevels[index].style.width = item.textContent;
+});
+
+
+
+// form script
+const overlay = document.querySelector('.overlay'),
+	form = document.querySelector('.contacts__form'),
+	modal = document.querySelector('.modal'),
+	modalClose = document.querySelector('.modal__close');
+
+
+const closeModal = (modalSelector) => {
+	modalSelector.classList.remove('active');
+}
+
+form.addEventListener('submit', (evt) => {
+	evt.preventDefault();
+	overlay.classList.add('active');
+	modal.classList.add('active')
+
+	setTimeout(() => {
+		modalClose.classList.add('active')
+	}, 1000)
+
+	setTimeout(() => {
+		closeModal(overlay);
+		closeModal(modal);
+		closeModal(modalClose)
+	}, 6000);
+
+	form.reset();
+})
+
+modalClose.addEventListener('click', () => {
+	closeModal(overlay);
+	closeModal(modal);
+	closeModal(modalClose)
+})
+
+document.addEventListener('keydown', (e) => {
+	if (e.code === "Escape") {
+		closeModal(overlay);
+		closeModal(modal);
+		closeModal(modalClose)
+	}
 });
