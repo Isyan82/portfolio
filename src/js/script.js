cec4923969a68b3d376e9bf4859fcaea3c9424e5
Isyan <isyan@mail.ru>
2022-04-25 02:26:14 +0300

    corrected scripts

--- src/js/script.js
+++ src/js/script.js
@@ -14,10 +14,9 @@ const openMenu = (modalSelector) => {
 
 gamburger.addEventListener('click', () => {
 	openMenu(menu);
-	// menu.classList.add('active');
+
 	setTimeout(() => {
 		openMenu(menuClose)
-		// menuClose.classList.add('active');
 	}, 1000)
 })
 menuClose.addEventListener('click', () => {
@@ -52,41 +51,116 @@ progressCounters.forEach((item, index) => {
 const overlay = document.querySelector('.overlay'),
 	form = document.querySelector('.contacts__form'),
 	modal = document.querySelector('.modal'),
+	modalText = document.querySelector('.modal__text'),
 	modalClose = document.querySelector('.modal__close');
+//=================================================================//
 
-
-const closeModal = (modalSelector) => {
+// функция закрытия элемента модального окна
+const closeModalElement = (modalSelector) => {
 	modalSelector.classList.remove('active');
 }
 
-form.addEventListener('submit', (evt) => {
-	evt.preventDefault();
+// функция показа модального окна с объявлением
+const showModal = (text) => {
+	modalText.innerHTML = text
 	overlay.classList.add('active');
-	modal.classList.add('active')
-
+	modal.classList.add('active');
+}
+// функция показа крестика
+const showModalClose = () => {
 	setTimeout(() => {
 		modalClose.classList.add('active')
-	}, 1000)
+	}, 1000);
+}
 
-	setTimeout(() => {
-		closeModal(overlay);
-		closeModal(modal);
-		closeModal(modalClose)
-	}, 6000);
+// функция отправки формы
+form.addEventListener('submit', (evt) => {
+	evt.preventDefault();
 
-	form.reset();
+	const formData = new FormData(form);
+	fetch('#',
+		{
+			method: 'POST',
+			body: formData,
+		})
+		.then((response) => {
+			if (response.ok === true) {
+				// Объявление об успешной оправке формы
+				showModal('Спасибо за вашу заявку! <br /> Я скоро с вами свяжусь.');
+				showModalClose();
+				setTimeout(() => {
+					closeModalElement(overlay);
+					closeModalElement(modal);
+					closeModalElement(modalClose)
+				}, 6000);
+				form.reset();
+			} else {
+				// Объявление о неуспешной оправке формы
+				showModal('Попробуйте позже, <br/>что-то пошло не так.')
+				showModalClose();
+				setTimeout(() => {
+					closeModalElement(overlay);
+					closeModalElement(modal);
+					closeModalElement(modalClose)
+				}, 6000);
+				form.reset();
+			}
+		})
+		.catch(() => {
+			showModal('Попробуйте позже, <br/>что-то пошло не так.')
+			showModalClose();
+			setTimeout(() => {
+				closeModalElement(overlay);
+				closeModalElement(modal);
+				closeModalElement(modalClose)
+			}, 6000);
+			form.reset();
+		})
 })
 
+// modal close script
 modalClose.addEventListener('click', () => {
-	closeModal(overlay);
-	closeModal(modal);
-	closeModal(modalClose)
+	closeModalElement(overlay);
+	closeModalElement(modal);
+	closeModalElement(modalClose)
 })
-
+// событие закрывания модального окна клавишей ESC
 document.addEventListener('keydown', (e) => {
 	if (e.code === "Escape") {
-		closeModal(overlay);
-		closeModal(modal);
-		closeModal(modalClose)
+		closeModalElement(overlay);
+		closeModalElement(modal);
+		closeModalElement(modalClose)
 	}
 });
+
+
+
+
+
+
+// page up script
+
+const pageup = document.querySelector('.pageup');
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 1300) {
+		pageup.classList.add('active')
+	} else {
+		pageup.classList.remove('active')
+	}
+})
+
+// плавная прокрутка по ссылкам
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substring(1)
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
